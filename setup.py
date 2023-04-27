a6ef879b67425a6177a81d9786c8a19ba7052235
OneNc <muhamadtriwanda@gmail.com>
2023-04-27 17:08:00 +0700

    Base Python Package

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='paydia',
+    version='0.1.0',    
+    description='A example Python package',
+    url='https://github.com/OneNc/paydia',
+    author='OneNc',
+    author_email='muhamadtriwanda@gmail.com',
+    license='MIT',
+    packages=['paydia'],
+    install_requires=[],
+
+    classifiers=[  
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
